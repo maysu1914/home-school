935682cd5a872f165404721ede50994fdef10b23
maysu1914 <aysum1914@gmail.com>
2021-03-15 13:09:38 +0300

    Change notification title name; Change whole logic structure; Show next lesson

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
-import datetime
 import locale
 import threading
 from concurrent.futures.thread import ThreadPoolExecutor
 
-# import vlc
 from win10toast import ToastNotifier
 
 from app.utils import *
@@ -13,7 +11,7 @@ class HomeSchool:
     """
     Alarm when the lesson approaches and ends.
     """
-    title = "Home School"
+    title = "Ev Okulu"
 
     def __init__(self, ders_programi_path, language="tr.UTF-8"):
         """
@@ -25,49 +23,61 @@ class HomeSchool:
         :param ders_programi_path: JSON file path of the syllabus
         """
         locale.setlocale(locale.LC_ALL, language)
-        # self.alarm_path = "resources/meb_okul_zili.mp3"
-        self.toaster = ToastNotifier()
-        self.ders_programi = get_dict_from_json_file(ders_programi_path)
         self.today = datetime.datetime.today().strftime("%A")
-        self.alarms = []
-        self.lesson_times = []
-        self.set_alarms()
-        self.set_lesson_times()
+        self.toaster = ToastNotifier()
         self.interval = 0.5
+        self.notification_duration = 30
+        self.ders_programi = self.get_today_syllabus(ders_programi_path)
+        self.last_lesson_time = max([value['bitis'] for key, value in self.ders_programi.items()])
+
+        self.last_lesson = {}
         self.wmi_thread = None
 
-    def set_alarms(self):
-        """
-        Read lesson clocks from syllabus,
-        set alarms 3 min before to lesson starts,
-        and lesson ends.
-        :return:
-        """
-        for key, value in self.ders_programi[self.today].items():
-            start = get_updated_now_by_given_date(value['baslangic'], '%H:%M')
-            before_start = start - datetime.timedelta(0, 0, 0, 0, 3)
-            finish = get_updated_now_by_given_date(value['bitis'], '%H:%M')
-            self.alarms.append({'type': 'before_start', 'lesson': key, 'time': before_start, 'status': False})
-            self.alarms.append({'type': 'start', 'lesson': key, 'time': start, 'status': False})
-            self.alarms.append({'type': 'finish', 'lesson': key, 'time': finish, 'status': False})
+        self.set_last_lesson()
 
-    def set_lesson_times(self):
-        """
-        Set lesson clocks for future usage,
-        do something while the lesson continues.
-        :return:
-        """
-        for key, value in self.ders_programi[self.today].items():
+    def get_today_syllabus(self, ders_programi_path):
+        syllabus = get_dict_from_json_file(ders_programi_path)[self.today]
+        for key, value in syllabus.items():
             start = get_updated_now_by_given_date(value['baslangic'], '%H:%M')
             finish = get_updated_now_by_given_date(value['bitis'], '%H:%M')
-            self.lesson_times.append((start, finish))
+            syllabus[key]['baslangic'] = start
+            syllabus[key]['bitis'] = finish
+            syllabus[key]['durum'] = 'pending'  # pending, near, started, finished
+        return syllabus
+
+    def set_last_lesson(self):
+        """
+        sets the last lesson data to individual variable
+        :return:
+        """
+        prev_lesson = {}
+        for key, value in self.ders_programi.items():
+            if value['baslangic'] < datetime.datetime.now() < value['bitis']:
+                self.ders_programi[key]['durum'] = 'started'
+                self.last_lesson = value
+                break
+            elif prev_lesson and datetime.datetime.now() > prev_lesson['bitis']:
+                self.last_lesson = prev_lesson
+            prev_lesson = value
+
+    def get_next_lesson(self):
+        """
+        returns the next lesson data if exist
+        :return:
+        """
+        if self.last_lesson:
+            next_lesson_id = str(self.last_lesson['id'] + 1)
+            if next_lesson_id in self.ders_programi:
+                return self.ders_programi[next_lesson_id]
+            else:
+                return None
 
     def run(self):
         """
         Loop until the last lesson is finished.
         :return:
         """
-        if not datetime.datetime.now() > self.alarms[-1]['time']:
+        if not datetime.datetime.now() > self.last_lesson_time:
             threading.Timer(self.interval, self.run).start()
             self.start()
 
@@ -84,55 +94,66 @@ class HomeSchool:
         Check alarm conditions
         :return:
         """
-        for index, alarm in zip(range(len(self.alarms)), self.alarms):
-            if not alarm['status']:
-                # try to call alarm before 2 seconds or in 2 seconds
-                time_difference = abs((datetime.datetime.now() - alarm['time']).total_seconds())
+        for key, value in self.ders_programi.items():
+            if value['durum'] == 'pending':
+                before_start = value['baslangic'] - datetime.timedelta(0, 0, 0, 0, 3)
+                if before_start < datetime.datetime.now() < value['baslangic']:
+                    self.ders_programi[key]['durum'] = 'near'
+                    self.call_alarm(value['durum'], lesson=value)
+            elif value['durum'] == 'near':
+                time_difference = abs((datetime.datetime.now() - value['baslangic']).total_seconds())
                 if time_difference < 2:
-                    self.alarms[index]['status'] = True
-                    self.call_alarm(self.ders_programi[self.today][alarm['lesson']], alarm['type'])
+                    self.ders_programi[key]['durum'] = 'started'
+                    self.last_lesson = value
+                    self.call_alarm(value['durum'])
+            elif value['durum'] == 'started':
+                time_difference = abs((datetime.datetime.now() - value['bitis']).total_seconds())
+                if time_difference < 2:
+                    self.ders_programi[key]['durum'] = 'finished'
+                    self.last_lesson = value
+                    self.call_alarm(value['durum'])
 
     def lesson_control(self):
         """
         Check lesson conditions
         :return:
         """
-        for index, lesson_time in zip(range(1, len(self.lesson_times) + 1), self.lesson_times):
-            if lesson_time[0] < datetime.datetime.now() < lesson_time[1]:
-                # print(self.ders_programi[self.today][str(index)])
+        if self.last_lesson:
+            if self.last_lesson['baslangic'] < datetime.datetime.now() < self.last_lesson['bitis']:
                 self.on_lesson()
                 return
         self.off_lesson()
         return
 
-    def call_alarm(self, lesson, type):
+    def call_alarm(self, type, lesson=None):
         """
         Calling alarm
+        :param lesson:
         :param type: Alarm type
-        :param lesson: Data of trigger lesson from syllabus
         :return:
         """
-        # vlc.MediaPlayer(self.alarm_path).play()
-        if type == 'before_start':
-            self.toaster.show_toast(title=self.title,
-                                    msg="%s dersiniz yaklaşıyor. %s\nKitaplarınızı hazırlayın." % (
-                                        lesson['ders'], lesson['baslangic']),
-                                    duration=30, threaded=True)
-        elif type == 'start':
-            self.toaster.show_toast(title=self.title,
-                                    msg="%s dersiniz başladı." % (lesson['ders']),
-                                    duration=30, threaded=True)
+        # we don't use built-in threaded paramter of toaster,
+        # because it has protection to multi notification in the same time
+        if type == 'near':
+            ThreadPoolExecutor().submit(self.toaster._show_toast, title=self.title,
+                                        msg="%s dersiniz yaklaşıyor. %s\nKitaplarınızı hazırlayın." % (
+                                            lesson['ders'], lesson['baslangic'].strftime('%H:%M')),
+                                        duration=self.notification_duration, icon_path=None)
+        elif type == 'started':
+            ThreadPoolExecutor().submit(self.toaster._show_toast, title=self.title,
+                                        msg="%s dersiniz başladı." % self.last_lesson['ders'],
+                                        duration=self.notification_duration, icon_path=None)
         else:
-            self.toaster.show_toast(title=self.title,
-                                    msg="%s dersiniz bitti. %s" % (lesson['ders'], lesson['bitis']),
-                                    duration=30, threaded=True)
+            ThreadPoolExecutor().submit(self.toaster._show_toast, title=self.title,
+                                        msg="%s dersiniz bitti." % self.last_lesson['ders'],
+                                        duration=self.notification_duration, icon_path=None)
 
     def on_lesson(self):
         """
         Do something when the lesson is ongoing.
         :return:
         """
-        # print('on lesson')
+        print('on lesson')
         trigger_process = 'CptHost.exe'  # Zoom meeting exe
         processes_to_kill = ['firefox.exe']
         if not self.wmi_thread:
@@ -145,9 +166,5 @@ class HomeSchool:
         Do something when no lesson.
         :return:
         """
-        print('out lesson')
-        # processes = {}  # process name and path
-        # if not self.wmi_thread:
-        #     self.wmi_thread = ThreadPoolExecutor().submit(start_process, processes)
-        # elif not self.wmi_thread.running():
-        #     self.wmi_thread = None
+        next_lesson = self.get_next_lesson()
+        print('out lesson', next_lesson['ders'] if next_lesson else 'There no upcoming lesson')
